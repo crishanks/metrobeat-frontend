dfae288aafe9934a9cb744cf2eccac3bfb7527ed
github username <crishanks12@gmail.com>
2019-04-11 14:07:31 -0700

    added working button for spotify oauth request

---
+++ src/Components/Button.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Button extends Component {
+  render() {
+    return (
+      <a href="http://localhost:3000/api/v1/login">
+      Log in to Spotify
+      </a>
+    )
+  }
+}
+
+export default Button

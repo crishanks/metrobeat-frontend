1d11798f3c7b8507e53eeb030b7314a39a825e71
github username <crishanks12@gmail.com>
2019-04-12 12:56:40 -0700

    Successfully fetched user and playlists, can save to state, access/refresh tokens now available.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,54 @@ import './App.css';
 
 import Button from './Components/Button'
 
+const API = 'https://api.spotify.com/v1/me/playlists'
+
 class App extends Component {
 
+  constructor() {
+    super()
+    this.state = {
+      users: [],
+      playlists: []
+    }
+  }
+
+  componentDidMount = () => {
+      this.fetchUser()
+    }
+
+    fetchUser = () => {
+      fetch('http://localhost:3000/api/v1/users')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({users: data})
+        console.log(this.state.users[0].access_token)
+        this.fetchPlaylists()
+      })
+    }
+
+    fetchPlaylists = () => {
+      fetch(API, {
+        headers: {
+          'Authorization': 'Bearer ' + this.state.users[0].access_token
+        }}
+      )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        this.setState({playlists: data})
+      })
+    }
+    
   render() {
     return (
       <div className="App">
         <h1>MetroBeat</h1>
+
         <Button />
+
+        <h3>User Information</h3>
+  
       </div>
     );
   }
